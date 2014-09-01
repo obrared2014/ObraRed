f3f960e542e69759e5d156ead247ae27b07dfc06
Ricardo <obraredchile@gmail.com>
2014-09-01 10:41:39 -0400

    Creé el archivo datosBD.php el que tiene los datos de que se usan para conectar a la base y así separarlo ya que lo uso en la búsqueda de materiales y se usan también en la búsque de materiales.

--- Modelo/Conexion.php
+++ Modelo/Conexion.php
@@ -1,11 +1,12 @@
 <?php 
-$server='localhost';
-$user='root';
-$pass='root';
+include("datosBD.php");
+//$server='localhost';
+//$user='root';
+//$pass='root';
 
-$con=mysql_connect($server,$user,$pass);
+$con=mysql_connect($servidor,$usuario,$contrasena);
 if($con){
-    $database=mysql_select_db("db_obrared");
+    $database=mysql_select_db($basedatos);
     $error=1;//Variable  que permite controlar si está conectado o no el valor 1 indica que está conectado
 }else{
     $error=2;//Variable que permite controlar si está conectado o no el valor 2 indica que no se conectó
---
+++ Modelo/datosBD.php
@@ -0,0 +1,15 @@
+<?php
+//$server='localhost';
+//$user='root';
+//$pass='root';
+    $servidor = "localhost";
+    $puerto = "3306";
+    $basedatos = "db_obrared";
+    $usuario = "root";
+    $contrasena = "root";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
