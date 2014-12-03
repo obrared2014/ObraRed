50364e0f8df86f9393ca132b2506ac9cd2c380a3
obrared2014 <obraredchile@gmail.com>
2014-12-03 19:33:37 -0300

    Ahora si los perfiles funcionan de choro

--- Vista/Cabecera.php
+++ Vista/Cabecera.php
@@ -44,9 +44,13 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Bienvenido <?php echo $_SESSION['nombre'].' '.$_SESSION['ap_paterno'] ?> <b class="glyphicon glyphicon-user"></b></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="?sec=Perfil"><b class="glyphicon glyphicon-cog"></b>&nbsp;Perfil</a></li>
-                        <li><a href="?sec=mantenedorMateriales"><b class="glyphicon glyphicon-briefcase"></b>&nbsp;Materiales</a></li> 
-                        <li><a href="?sec=mantenedorUsuarios"><b class="glyphicon glyphicon-floppy-open"></b>&nbsp;Usuarios</a></li>  
-                        <li><a href="?sec=Agrega_Materiales"><b class="glyphicon glyphicon-floppy-open"></b>&nbsp;Agregar Materiales</a></li>  
+                        <?php if($_SESSION['perfil']=='ADMIN'){ ?>
+                            <li><a href="?sec=mantenedorMateriales"><b class="glyphicon glyphicon-briefcase"></b>&nbsp;Materiales</a></li> 
+                            <li><a href="?sec=mantenedorUsuarios"><b class="glyphicon glyphicon-floppy-open"></b>&nbsp;Usuarios</a></li>
+                        <?php } ?>
+                        <?php if($_SESSION['perfil'] != 'USUARIO' ){ ?>
+                            <li><a href="?sec=Agrega_Materiales"><b class="glyphicon glyphicon-floppy-open"></b>&nbsp;Agregar Materiales</a></li>  
+                        <?php } ?>
                         <li><a href="Controlador/Logout.php"><b class="glyphicon glyphicon-off"></b>&nbsp;Cerrar Sesión</a></li>
                     </ul>
                 </li>
